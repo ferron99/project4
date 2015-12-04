0ca4be55bd10bea33f99cadda663366f4f9ad083
ferron99 <nferro@terpmail.umd.edu>
2015-12-04 10:39:40 -0500

    Update Project_4_WIP.java

--- Project_4_WIP.java
+++ Project_4_WIP.java
@@ -1,66 +1,77 @@
 ////Nick Ferro
-//// Project 4   11-19-15    
-//// Pool table elastic collisions
-////  arrays
-Ball[] balls;
+//// Project 4   12-1-15    
+//// WIP
+Ball[] fer;
+Button[] nic;
+float[] dist = new float[15]; 
+float[] Ycoor = new float[15];
 PTable t;
-Button one, two, three, four;
-Bird brd;
-Rat rt;
-Cloud[] cloudList;
-int frame;
-int score;
+
 
 //SETUP: object stuff
 void setup() {
-  size( 720, 480 );
+  size( 1200, 675 );
   t= new PTable();
-  t.left=   50;
-  t.right=  width-50;
-  t.top=    150;
-  t.bottom= height-50;
+  t.left=   75;
+  t.right=  width-300;
+  t.top=    200;
+  t.bottom= height-75;
   t.middle= t.left + (t.right-t.left) / 2;
   t.horizon= (height/4)-20;
   t.wall=true;
   //
-  balls= new Ball[5];
-  for (int i = 0; i < balls.length; i++) {
-    balls[i] = new Ball(random(t.middle+15,t.right-15), random(t.top+15,t.bottom+15), 30, i, balls);
+  fer= new Ball[16];
+  for (int i = 0; i < 1; i++) {
+    fer[i] = new Ball(width/4, (t.bottom+t.top)/2, 30, i, fer);
   }
-  //
-  one= new Button(50,5);
-  one.words= "Reset";
-  //
-  two= new Button(140,5);
-  two.words= "Wall";
-  //
-  three= new Button(230,5);
-  three.words= "Bird";
-  //
-  four= new Button(320,5);
-  four.words= "Rat";
-  //
-  brd= new Bird();
-  brd.y = 70;
-  brd.by = 75;
-  //
-  rt= new Rat();
-  //
-  cloudList= new Cloud[7];
-  float cloudX=50;
-  for( int i=0; i<cloudList.length; i++) {
-     cloudList[i]=  new Cloud( cloudX,random(t.horizon-100,t.horizon-25));
-     cloudX += 100;
+  fer[0].dx = 0;
+  fer[0].dy = 0;
+  fer[0].r = 255;
+  fer[0].g = 255;
+  fer[0].b = 255;  
+  for (int i = 1; i < fer.length; i++) {
+    fer[i] = new Ball(random(t.middle+20,t.right-15), random(t.top+15,t.bottom-15), 30, i, fer);
   }
-  //
-  reset();
+  
+  for (int i=0; i < dist.length; i++) {
+    dist[i] = 0;
+  }
+  for (int i=0; i < Ycoor.length; i++) {
+    Ycoor[i] = 0;
+  }
+  
+  nic= new Button[7];
+  float buttonX=50;
+  for (int i=0; i < nic.length; i++) {
+    nic[i]= new Button(buttonX, 5);
+    buttonX+=100;
+  }
+  nic[0].words= "Reset";
+  nic[1].words= "Wall";
+  nic[2].words= "Bird";
+  nic[3].words= "Rat";
+  nic[4].words= "Closest";
+  nic[5].words= "List";
+  nic[6].words= "Sort";
+    
+  //reset();
 }
 
 //RESET function for balls
-void reset() {
-  
+void reset() { 
   t.wall = true;
-  
+  for (Ball b: fer){
+    b.reset();
+    b.resetCheck();
+  }
+  resetCue();
+}
+
+void resetCue(){
+  fer[0].x = width/4;
+  fer[0].y = (t.bottom+t.top)/2;
+  fer[0].dx = 0;
+  fer[0].dy = 0;
 }
 
 
@@ -68,63 +79,147 @@ void reset() {
 void draw() {
   background( 102,178,205 );
   t.tableDisplay();
-  drawGrass();
-  drawClouds();
-  for (Ball ball : balls) {
-    ball.collide();
-    ball.move();
-    ball.show();  
+  for (Ball b : fer) {
+    //b.resetCheck();
+    b.collide();
+    b.move();
+    b.show();  
   }
   buttons();
-  birds();
-  rats();
-  frame +=1;
-  showScore();
+  for (int i=0; i < dist.length; i++) {
+    dist[i] = dist(fer[i+1].x, fer[i+1].y, fer[0].x, fer[0].y);
+  }
+  for (int i=0; i < Ycoor.length; i++) {
+    Ycoor[i] = fer[i+1].y;
+  }
+
+  
+  //
+  fill(0);
+  textSize(15);
+  show( dist, dist.length, width-260, t.horizon+25);
+  closest();
+  //srt(Ycoor, Ycoor.length);
+  text( smallY(), 50, 100);
+  text( whereY()+1, 50, 125);
 }
 
-void drawGrass(){
-  int x = 0;
-  strokeWeight(2);
-  while (x <= width){
-    strokeWeight(1);
-    line(x, height, random(x,x+2), height-15);
-    x +=5;
+int whereClose( float a[], int m ) {
+  int k=0;
+  for (int i=1; i<m; i++) {
+    if( a[i] < a[k] )  {
+      k=  i;
+    }
+  }
+  return k;
+}
+
+int whereY() {
+  int k=0;
+  for (int i=1; i<Ycoor.length; i++) {
+    if( Ycoor[i] < Ycoor[k] )  {
+      k=  i;
+    }
+  }
+  return k;
+}
+
+float smallY() {
+  float smY;
+
+  smY=  Ycoor[0];
+  for (int i=1; i<Ycoor.length; i++) {
+    if( Ycoor[i] < smY )  {
+      smY=  Ycoor[i];
+    }
+  }
+
+  return smY;
+}
+
+void srt( float a[], int many ) {
+ 
+  int m=  many;
+  int j;
+  int k;        // index of closest Y
+ 
+  for (m=many; m>1; m=m-1 ) { 
+    k=  whereY();
+    j=  m-1;
+    swap( Ycoor, j, k );
+  }
+
+}
+void swap( float a[], int j, int k ) {
+  float tmp;
+  tmp=  a[k];
+  a[k]=  a[j];
+  a[j]=  tmp;
+}
+
+void closest(){
+  if (nic[4].ring == true){
+    fill(0);
+    strokeWeight(4);
+    stroke(255);
+    rect((width/2)-10, 100, 175, 50);
+    int close;
+    close = whereClose(dist, dist.length)+1;
+    fill(fer[close].r,fer[close].g,fer[close].b);
+    text("The closest ball is "+close, width/2, 125);
+    noFill();
+    stroke(255);
+    ellipse(fer[close].x, fer[close].y, 32,32);
   }
 }
 
-void drawClouds(){
-   for (int i=0; i<cloudList.length; i++) {
-    cloudList[i].showClouds();
-  }
-}
 
 
-//DISPLAY BUTTONS
+
 void buttons(){
-  one.buttonDisplay();
-  two.buttonDisplay();
-  three.buttonDisplay();
-  three.buttonBirdBuffer();
-  four.buttonDisplay();
+  for (int i=0; i<nic.length; i++) {
+    nic[i].buttonDisplay();
+  }
 }
 
-void birds(){
-  brd.moveBird();
-  brd.showBird();
-  brd.bombDrop();
-  brd.explosion();
-}
-
-void rats(){
-  rt.moveRat();
-  rt.showRat();
-  rt.scoreFix();
-}
-
-void showScore(){
-  text("Score", 50, height-50);
-  text(score , 90, height -50);
-}
+void show(float a[], int m, float x, float y ) {
+  if (nic[5].list == true){
+    float ytmp;
+    ytmp = y;
+    for (int i=0; i<m; i++) {
+      text( i+1, x, ytmp );
+      ytmp += 15;
+    }
+    x += 25;
+    ytmp = y;
+    for (int i=0; i<m; i++) {
+      int rx = round(fer[i+1].x);
+      text( rx, x, ytmp );
+      ytmp += 15;
+    }
+    x += 35;
+    ytmp = y;
+    for (int i=0; i<m; i++) {
+      int ry = round(fer[i+1].y);
+      text( ry, x, ytmp );
+      ytmp += 15;
+    }
+    x += 35;
+    ytmp = y;
+    for (int i=0; i<m; i++) {
+      int ry = round(dist[i]);
+      text( ry, x, ytmp );
+      ytmp += 15;
+    }
+    x += 50;
+    ytmp = y;
+    for (int i=0; i<m; i++) {
+      int ry = round(Ycoor[i]);
+      text( ry, x, ytmp );
+      ytmp += 15;
+    }
+  }
+}  
 
 // KEY PRESSED stuff: exit, reset, other buttons
 void keyPressed() {
@@ -133,24 +228,23 @@ void keyPressed() {
   if (key == 'w') t.wall = false;
 }
 
-//MOUSEPRESSED: hits for the button functions
 void mousePressed() {
- one.buttonReset();
- two.buttonWall();
- three.buttonBird();
- four.buttonRat();
-
- rt.clickRat();
+  nic[0].buttonReset();
+  nic[1].buttonWall();
+  
+  
+  nic[4].buttonClose();
+  nic[5].buttonList();
 }
-
+  
 
 //BALL CLASS
 class Ball {
   // PROPERTIES
   float x,y;
   float diameter;
-  float dx;
-  float dy;
+  float dx, dy;
+  float r,g,b;
   int id;
   Ball[] others;
 
@@ -162,10 +256,13 @@ class Ball {
     others = oin;
     dx=random(-2,2);
     dy=random(-2,2);
+    r=random(0,255);
+    g=random(0,255);
+    b=random(0,255);
   }
   // METHODS
   void collide(){
-    for (int i = id+1; i < balls.length; i++) {
+    for (int i = id+1; i < fer.length; i++) {
       float distance = dist(x,y,others[i].x,others[i].y);
       if (distance < diameter) {
         float tmp;
@@ -177,8 +274,10 @@ class Ball {
   void show() {
     stroke(0);
     strokeWeight(1);
-    fill(0);
+    fill(r,g,b);
     ellipse( x,y, diameter, diameter );
+    fill(0);
+    text(id,x-5,y);
   }
   
   void move() {
@@ -191,8 +290,22 @@ class Ball {
     x=  x+dx;
     y=  y+dy;
   }
-}
+  void reset(){
+    x=random(t.middle+20,t.right-15); 
+    y=random(t.top+15,t.bottom-15);
+    dx=random(-2,2);
+    dy=random(-2,2);
+  }
   
+  void resetCheck(){
+     for(int i = id+1; i < fer.length; i++){
+     float distance = dist(x,y,others[i].x,others[i].y);
+     if (distance < diameter) {
+       reset();
+     }
+   }
+  }
+}
 
 //PTABLE CLASS
 class PTable {
@@ -205,7 +318,7 @@ class PTable {
   void tableDisplay(){
     noStroke();
     fill( 222,184,135 );
-    rect( 0,horizon, width,(height*3/4)+25 );
+    rect( 0, horizon, width, height );
     fill( 100, 250, 100 );    
     strokeWeight(20);
     stroke( 127, 0, 0 );      // walls of pool talbe
@@ -222,252 +335,21 @@ class PTable {
   }
 } 
 
-class Bird {
-  float x,y,by,bDY,exX,exY;      //x and y for bird, by is bomb's y, bDY si bomb speed, exX and exY for explosion animation
-  boolean fly, drop, explode;
-  int exFrame;
-  
-  Bird() {
-    fly = false;
-    drop = false;
-    x = -50;
-    bDY = 2;
-  }
-  // movement for the bird. when the bird crosses the right edge of the screen, it 
-  // resets a ton of variables all tied to the bird flight and bomb droping mechanic
-  void moveBird(){
-    if (fly)
-    x +=4;
-    if (x>width+50){
-      x= -50;
-      fly = false;             //causes the bird to fly when true
-      drop =false;             //causes the bomb to drop when true
-      three.counter = false;   //when true, starts the buffer counter
-      three.buffer = 0;        //counts up when counter is true, enables drop to use the same button as fly on subsequent clicks
-      by = 70;
-      bDY = 2;
-      
-    }
-  }
-  //Animation and display for birds. Frame count is for wing animation
-  void showBird(){                 
-    stroke(0);
-    strokeWeight(1);
-    fill(192);
-    ellipse(x,y,35,13);
-    ellipse(x+17, y-5, 15,10);
-    triangle(x+24, y-6, x+30, y-4, x+24, y-2);
-    triangle(x-18, y-2, x-30, y-4, x-19, y);
-    if(frame/15 % 4 == 0){
-      triangle(x-6, y-3, x+18, y-17, x+5, y);
-    }else if(frame/15 % 4 == 1){
-      triangle(x-7, y-3, x, y-1, x+6, y);
-    }else if(frame/15 % 4 == 2){
-      triangle(x-6, y-3, x-18, y+17, x+5, y);
-    }else if(frame/15 % 4 == 3){
-     triangle(x-4, y-8, x-13, y+7, x+7, y-5);
-    }
-  }
-  //draws bomb and sends it downward with increasing velocity
-  void bombDrop(){
-    if (drop == true){
-        //noStroke();
-        fill(105);
-        rect(x,by,10,10);
-        ellipse(x+5,by,10,10);
-        ellipse(x+5,by+10,10,10);
-        triangle(x+5,by, x,by-13, x+10,by-13);
-        by += bDY;
-        bDY += .25;
-    }
-    if (dist(x,by,rt.x,rt.y)<40){
-      drop = false;
-      rt.crawl = false;
-      explode = true;
-      exFrame = 0;
-      exX=x;
-      exY=by;
-      score +=100;
-      rt.x = -50;
-      rt.y = random(170,height-50);
-  
-      
-    }
-  }
-  
-  //animation for bomb explosion
-  
-  void explosion(){
-    if (explode == true){
-      exFrame = exFrame +1;
-      if (exFrame == 1){
-        fill(255,255,0);
-        ellipse(exX,exY,12,12);
-      }else if(exFrame == 2){
-        fill(255);
-        ellipse(exX,exY,24,24);
-      }else if(exFrame == 3){ 
-        fill(255,255,0);
-        ellipse(exX,exY,36,36);
-      }else if(exFrame == 4){ 
-        fill(255);
-        ellipse(exX,exY,48,48);
-      }else if(exFrame == 5){ 
-        fill(255,255,0);
-        beginShape();
-        vertex(exX-24,exY);
-        bezierVertex(exX-24,exY-13,exX-13,exY-24,exX,exY-24);   //outer top left quadrent
-        vertex(exX,exY-24);
-        bezierVertex(exX+13,exY-24,exX+24,exY-13,exX+24,exY);   //outer top right quadrant
-        vertex(exX+24,exY);
-        bezierVertex(exX+24,exY+13,exX+13,exY+21,exX+3*sqrt(15),exY+21);  ////outer bottom right quadrant
-        vertex(exX+3*sqrt(15),exY+21);
-        bezierVertex(exX+12,exY+17,exX+7,exY+12,exX,exY+12);  //inner right
-        vertex(exX,exY+12);
-        bezierVertex(exX-7,exY+12,exX-12,exY+17,exX-3*sqrt(15),exY+21); //inner left
-        vertex(exX-3*sqrt(15),exY+21);
-        bezierVertex(exX-13,exY+21,exX-24,exY+13,exX-24,exY);     //outer bottom left
-        endShape(CLOSE);
-      }else if(exFrame == 6){ 
-        fill(255);
-        beginShape();
-        vertex(exX-24,exY);
-        bezierVertex(exX-24,exY-13,exX-13,exY-24,exX,exY-24);   //outer top left quadrent
-        vertex(exX,exY-24);
-        bezierVertex(exX+13,exY-24,exX+24,exY-13,exX+24,exY);   //outer top right quadrant
-        vertex(exX+24,exY);
-        bezierVertex(exX+24,exY+13,exX+13,exY+16,exX+18,exY+16);  ////outer bottom right quadrant
-        vertex(exX+18,exY+16);
-        bezierVertex(exX+18,exY+6,exX+10,exY-2,exX,exY-2);      //inner right
-        vertex(exX,exY-2);
-        bezierVertex(exX-10,exY-2,exX-18,exY+6,exX-18,exY+16);  //inner left
-        vertex(exX-18,exY+16);
-        bezierVertex(exX-13,exY+21,exX-24,exY+13,exX-24,exY);  //outer bottom left
-        endShape(CLOSE);
-      }else if(exFrame == 7){ 
-        fill(255,255,0);
-        beginShape();
-        vertex(exX-24,exY);
-        bezierVertex(exX-24,exY-13,exX-13,exY-24,exX,exY-24);   //outer top left quadrent
-        vertex(exX,exY-24);
-        bezierVertex(exX+13,exY-24,exX+24,exY-13,exX+24,exY);   //outer top right quadrant
-        vertex(exX+24,exY);
-        bezierVertex(exX+24,exY-9,exX+13,exY-16,exX,exY-16);  //inner right
-        vertex(exX,exY-16);
-        bezierVertex(exX-13,exY-16,exX-24,exY-9,exX-24,exY);    //inner left
-        endShape(CLOSE);
-        explode = false;
-      }
-    }
-  }
-}
-
-class Rat {
-  
-  float x,y,DX,DY;
-  boolean crawl;
-  boolean scoreBuffer;
-  int scoreBufferCounter;
-  
-  Rat() {
-    crawl = false;
-    scoreBuffer = false;
-    x = -50;
-    y = random(170,height-50);
-  }
-  
-  void moveRat(){
-    if (crawl){
-      DX=random(0,5);
-      DY=random(-5,5);
-      x+=DX;
-      y+=DY;
-      if (x>width+50){
-        crawl = false;
-        x = -50;
-        y = random(170,height-50);
-      }
-    }
-  }
-  
-  void showRat(){
-    stroke(1);
-    fill(210,180,140);
-    ellipse(x,y,30,15);
-    ellipse(x+15,y,15,13);
-    fill(209,193,173);
-    ellipse(x+10,y-5,10,5);
-    ellipse(x+10,y+5,10,5);
-    fill(0);
-    ellipse(x+22,y,5,5);
-    noFill();
-    if (frame/15 % 2 == 0){
-      arc(x-22,y,15,15,0,PI);
-      arc(x-37,y,15,15,PI, TWO_PI);
-    }else if(frame/15 % 2 == 1){
-      arc(x-22,y,15,15,PI,TWO_PI);
-      arc(x-37,y,15,15,0,PI); 
-    }
-  }
-  void clickRat(){
-    if (dist(mouseX,mouseY,x,y)<20){
-         crawl = false;
-        x = -50;
-        y = random(170,height-50);
-        score +=50;
-    }
-  }
-      
-  void scoreFix(){
-    if (scoreBuffer == true){
-      scoreBufferCounter +=1;
-      if (scoreBufferCounter>10){
-        scoreBuffer = false;
-      }
-    }
-  }  
-}
-
-class Cloud{
-  
-  float x, y;            
-  
-  Cloud( float x, float y){
-    this.x=x;  this.y=y;
-  }
- 
-  void showClouds() {
-    stroke(0);
-    fill(255);
-    arc(x,y,30,30,HALF_PI,PI+HALF_PI+QUARTER_PI);
-    arc(x+15,y-15,30,30,HALF_PI+QUARTER_PI,TWO_PI+QUARTER_PI);
-    arc(x+30,y-15,30,30,HALF_PI+QUARTER_PI,TWO_PI+QUARTER_PI);
-    arc(x+45,y,30,30,PI+QUARTER_PI,TWO_PI+HALF_PI);
-    line(x,y+15,x+45,y+15);
-    noStroke();
-    ellipse(x+22,y-16,25,25);
-    ellipse(x+11,y-7,25,25);
-    ellipse(x+33,y-7,25,25);
-    rectMode(CORNER);
-    rect(x-1,y-1,47,15);
-    x++;
-    if (x>width+10){
-      x= random(-200,-100);
-    }
-  }
-}
-
 class Button {
   //PROPERTIES
   float x,y;
   String words;
   boolean counter;
+  boolean ring;
+  boolean list;
   int buffer;
   //CONSTRUCTOR
   Button(float tempX, float tempY) {
     x = tempX;
     y = tempY;
     counter = false;
+    ring = false;
+    list = false;
     buffer = 0;
   }
   //METHODS
@@ -492,29 +374,38 @@ class Button {
     t.wall = false;
   }
  }
+ void buttonClose(){
+  if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+    ring = true;
+  }
+ }
+ void buttonList(){
+  if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+    list = true;
+  }
+ }
  //multifunctional button. Sends the bird flying on the first click
  //and drops a bomb on the second click, then does nothing untill the
  //bird completes its flight.
- void buttonBird(){
-   if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
-     brd.fly = true;
-     counter = true;
-     if (buffer > 1){
-       brd.drop = true;
-     }
-   }
- }
+ //void buttonBird(){
+ //  if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+ //    brd.fly = true;
+ //    counter = true;
+ //    if (buffer > 1){
+ //      brd.drop = true;
+ //    }
+ //  }
+ //}
  //Buffer counter for the bird button. Button will only enable drop if at least
  //two frames have passed since fly was enabled.
- void buttonBirdBuffer(){
-   if (counter == true) {
-     buffer +=1;
-       }   
-     }
- void buttonRat(){
-   if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
-     rt.crawl = true;
-   }
- }
+ //void buttonBirdBuffer(){
+ //  if (counter == true) {
+ //    buffer +=1;
+ //      }   
+ //    }
+ //void buttonRat(){
+ //  if (mouseX >x && mouseX<x+80 && mouseY>y && mouseY<y+40){
+ //    rt.crawl = true;
+ //  }
+ //}
 }
- 
